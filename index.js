8a62576b42d7e9cf4911bb94ecdcc1e291594058
AbhayRajSingh23 <arajsingh609@gmail.com>
2025-10-04 01:17:51 +0530

    json package fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 3000;
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
